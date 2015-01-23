662924745f17352bb19e3eb86a8788fc4509b147
Jiaxin.Lin <Jiaxin.Lin@MU-C3-10.mullen.pvt.k12.co.us>
2015-01-23 13:41:24 -0700

    first draft

---
+++ 2nd tri project/src/Animal.java
@@ -0,0 +1,26 @@
+
+public class Animal
+	{
+	protected double babyP;
+	protected String name; 
+	protected double foodP;
+	protected double sellP;
+	protected int originalName;
+	
+	
+	
+	public void getAnimal()
+		{
+		System.out.println("You got an animal!");
+		
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+
+	}
---
+++ 2nd tri project/src/Mammal.java
@@ -0,0 +1,11 @@
+
+public abstract class Mammal extends Animal
+	{
+		public void getAnimal()
+			{
+			System.out.println("You got a little baby");
+			
+			}
+		public abstract void changeName(); 
+		
+	}
