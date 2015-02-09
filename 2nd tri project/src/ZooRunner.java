57b09785fde471dc9909b19530ad1ff48672042c
jiaxinlin <jiaxinlin@10.0.0.144>
2015-02-08 17:51:47 -0700

    ..

---
+++ 2nd tri project/src/ZooRunner.java
@@ -0,0 +1,28 @@
+import java.util.*; 
+public class ZooRunner
+	{
+	private static double start= 1000.0; 
+	private static int day=0; 
+	private static int animals=0; 
+	
+	
+    
+	
+	public static void main(String[] args)
+		{
+		// 50 is the cheapest animal to get. As long as there are animals on the field and have money, game continues. 
+		ArrayList zoo= new ArrayList(); 
+		while(start>50||animals>0)
+		{
+			System.out.println("What animal do you want to buy?");
+			System.out.println("1. fish.");
+			System.out.println("2. bird.");
+			System.out.println("3. pig. ");
+			System.out.println("4. cat. "); 
+			
+		}
+		
+
+		}
+
+	}
